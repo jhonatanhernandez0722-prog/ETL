a0b2f9c6893f2e1134a691802c0dc9e91d19b568
nathan <jhonatanhernandez0722@gmail.com>
2026-06-13 13:59:38 -0500

    Fix: Usar framework Express nativamente en Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,11 @@
+// Este archivo redirige todo a Express en server.js
 const app = require('../server');
+
+// Vercel Serverless Functions esperan un manejador (req, res)
+// pero Express es una app que necesita ser montada directamente
+// Por eso no podemos simplemente exportar app
+
+// Solución: permitir que el archivo sea ignorado y que Vercel
+// use la configuración en vercel.json para pasar todo a server.js
 
 module.exports = app;
