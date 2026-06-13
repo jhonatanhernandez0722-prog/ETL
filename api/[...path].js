ce3c9a73887b00564d4cbda8f35cfd005a5097bc
nathan <jhonatanhernandez0722@gmail.com>
2026-06-13 14:06:15 -0500

    Remove: Eliminar función serverless

--- api/[...path].js
+++
@@ -1,9 +0,0 @@
-// Este archivo redirige todo a Express en server.js
-const app = require('../server');
-
-// Vercel espera un manejador exportado, no una aplicación Express
-// Debemos exportar una función que Vercel pueda invocar
-module.exports = (req, res) => {
-  // Pasar la solicitud a Express
-  return app(req, res);
-};
