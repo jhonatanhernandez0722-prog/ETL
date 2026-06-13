d3de8163cc32c9eb3101487df488943d30b854e8
nathan <jhonatanhernandez0722@gmail.com>
2026-06-13 14:04:51 -0500

    Fix: Exportar función manejadora correcta en Vercel serverless

--- api/[...path].js
+++ api/[...path].js
@@ -1,11 +1,9 @@
 // Este archivo redirige todo a Express en server.js
 const app = require('../server');
 
-// Vercel Serverless Functions esperan un manejador (req, res)
-// pero Express es una app que necesita ser montada directamente
-// Por eso no podemos simplemente exportar app
-
-// Solución: permitir que el archivo sea ignorado y que Vercel
-// use la configuración en vercel.json para pasar todo a server.js
-
-module.exports = app;
+// Vercel espera un manejador exportado, no una aplicación Express
+// Debemos exportar una función que Vercel pueda invocar
+module.exports = (req, res) => {
+  // Pasar la solicitud a Express
+  return app(req, res);
+};
